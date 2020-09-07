f96b7b72ab2b236ba698ff92a9d69cc7e9adb8b1
Kevin Miller <kmillergit@outlook.com>
2020-09-07 07:40:58 -0700

    corrected stylesheet links

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,25 +1,29 @@
 const Burgers = require("../models/burger_model.js");
+const path = require("path")
+// if no burgers are already in database, display these default values instead
+const nullBurger = [
+  {
+    burger_name: "Your burgers here!",
+    devoured: false
+  },
+  {
+    burger_name: "Devoured burgers here!",
+    devoured: true
+  }
+]
+
 
 module.exports = function(app) {
+
 
   // should be last route
   app.get("/", function(req, res) {
     Burgers.findAll({}).then(function(result) {
-      console.log("HI")
       console.log(result);
-      const nullBurger = [
-        {
-          burger_name: "Your burgers here!",
-          devoured: false
-        },
-        {
-          burger_name: "Devoured burgers here!",
-          devoured: true
-        }
-      ]
+      // display nullBurger values if database is empty
       result === [] ? result = nullBurger : null;
 
-      // initialize empty arrays to hold burger objects
+      // initialize empty burger arrays to hold burger objects
       let undevouredArray = [];
       let devouredArray = [];
 
@@ -27,6 +31,7 @@ module.exports = function(app) {
         // create the burger objects from the query results
         let undevouredBurger = {};
         let devouredBurger = {};
+        // then push objects to the burger arrays
         if (result[i]["devoured"] === false) {
           undevouredBurger["burger_name"] = result[i]["burger_name"];
           undevouredBurger["devoured"] = false;
@@ -38,10 +43,11 @@ module.exports = function(app) {
           devouredArray.push(devouredBurger);
         }
       }
+      // finally, use handlebars to render the arrays
       res.render("index", {
         burgArray : undevouredArray,
         devouredBurgArray : devouredArray
       });
-    }).then()
+    });
   });
 }
