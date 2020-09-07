256a26db8c61abfde19110f4defc430a1dd0a6c1
Kevin Miller <kmillergit@outlook.com>
2020-09-07 11:06:06 -0700

    added regex for burger name

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const Burgers = require("../models/burger_model.js");
 
+
 // here, the parameter app will be express()
 module.exports = function(app) {
 
@@ -10,6 +11,12 @@ module.exports = function(app) {
   });
 
   app.post("/api/burgers/new", function(req, res) {
+    // req.body["burger_name"].trim();
+    console.log(req.body)
+    if (!(/burger/i).test(req.body["burger_name"])) {
+      req.body["burger_name"] = req.body["burger_name"].trim() + " burger";
+    }
+
     Burgers.create(req.body).then((result) => {
       console.log(req.body)
       res.status(200).end();
