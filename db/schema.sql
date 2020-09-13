520d1e9eaa813be638d03a51ee9183872f470da1
Kevin Miller <kmillergit@outlook.com>
2020-09-13 16:59:19 -0700

    trying to get heroku database working

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("extremely regular burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("irregular burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("ornery burger", false);
